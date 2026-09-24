3dfbb6802b554e9b5ce75ca0359ddf28de015245
lb36-314 <laronbrown338@gmail.com>
2026-09-23 19:40:11 -0500

    Lock down inspection template RPC execution

---
+++ supabase/migrations/20260924100000_lock_down_inspection_template_rpc.sql
@@ -0,0 +1,2 @@
+revoke execute on function public.ensure_1000_point_inspection_template(uuid) from anon, authenticated;
+grant execute on function public.ensure_1000_point_inspection_template(uuid) to service_role;
